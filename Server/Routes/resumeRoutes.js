a50c064b26591cb076dc1a75d29e6664f37bd517
Rashi <166101281+Rashi-Dang@users.noreply.github.com>
2025-07-04 19:06:38 +0530

    Update resumeRoutes.js

--- Server/Routes/resumeRoutes.js
+++ Server/Routes/resumeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createOrUpdateResume, getResume } = require('../controllers/resumeController');
+const { createOrUpdateResume, getResume } = require('../Controllers/resumeController');
 
 // POST (create or update)
 router.post('/', createOrUpdateResume); // actually does create + update humne controller m findone use kia age koi hoga to update krega nhi to create
